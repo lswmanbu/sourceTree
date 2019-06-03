803c740736172126eed3a187dc68efe9a05786bd
liushuwang <2402242332@qq.com>
2019-06-03 16:53:55 +0800

    master提交

---
+++ src/main/java/per/lsw/controller/AppController.java
@@ -0,0 +1,15 @@
+package per.lsw.controller;
+
+/**
+ * @ClassName : AppController
+ * @Description: TODO
+ * @Create Time: 2019/6/3 16:51
+ * @Author Name: liusw
+ * @Version : 1.0
+ */
+public class AppController {
+	
+	public void test() {
+		System.out.println("test...");
+	}
+}
