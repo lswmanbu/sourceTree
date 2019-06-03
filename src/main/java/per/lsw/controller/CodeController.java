d3f186ceef3233ef2ad39e4b198e7d3fd3a81463
liushuwang <2402242332@qq.com>
2019-06-03 16:57:16 +0800

    code branch push

---
+++ src/main/java/per/lsw/controller/CodeController.java
@@ -0,0 +1,15 @@
+package per.lsw.controller;
+
+/**
+ * @ClassName : CodeController
+ * @Description: TODO
+ * @Create Time: 2019/6/3 16:56
+ * @Author Name: liusw
+ * @Version : 1.0
+ */
+public class CodeController {
+	
+	public void code(){
+		System.out.println("code is running...");
+	}
+}
